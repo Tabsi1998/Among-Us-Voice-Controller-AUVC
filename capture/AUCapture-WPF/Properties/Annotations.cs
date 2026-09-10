a9fdfe6f07ee15e867178522375e2c8656d697da
Fabian <f.tabelander@gannomat.at>
2026-09-10 12:55:33 +0200

    style: remove trailing whitespace from C# XML comments

--- capture/AUCapture-WPF/Properties/Annotations.cs
+++ capture/AUCapture-WPF/Properties/Annotations.cs
@@ -39,7 +39,7 @@ namespace AUCapture_WPF.Annotations
     /// </summary>
     /// <example><code>
     /// [CanBeNull] object Test() => null;
-    /// 
+    ///
     /// void UseTest() {
     ///   var p = Test();
     ///   var s = p.ToString(); // Warning: Possible 'System.NullReferenceException'
@@ -112,7 +112,7 @@ namespace AUCapture_WPF.Annotations
     /// <example><code>
     /// [StringFormatMethod("message")]
     /// void ShowError(string message, params object[] args) { /* do something */ }
-    /// 
+    ///
     /// void Foo() {
     ///   ShowError("Failed: {0}"); // Warning: Non-existing argument in format string
     /// }
@@ -264,12 +264,12 @@ namespace AUCapture_WPF.Annotations
     /// <example><code>
     /// public class Foo : INotifyPropertyChanged {
     ///   public event PropertyChangedEventHandler PropertyChanged;
-    /// 
+    ///
     ///   [NotifyPropertyChangedInvocator]
     ///   protected virtual void NotifyChanged(string propertyName) { ... }
     ///
     ///   string _name;
-    /// 
+    ///
     ///   public string Name {
     ///     get { return _name; }
     ///     set { _name = value; NotifyChanged("LastName"); /* Warning */ }
@@ -388,7 +388,7 @@ namespace AUCapture_WPF.Annotations
     /// <example><code>
     /// [CannotApplyEqualityOperator]
     /// class NoEquality { }
-    /// 
+    ///
     /// class UsesNoEquality {
     ///   void Test() {
     ///     var ca1 = new NoEquality();
@@ -409,7 +409,7 @@ namespace AUCapture_WPF.Annotations
     /// <example><code>
     /// [BaseTypeRequired(typeof(IComponent)] // Specify requirement
     /// class ComponentAttribute : Attribute { }
-    /// 
+    ///
     /// [Component] // ComponentAttribute requires implementing IComponent interface
     /// class MyComponent : IComponent { }
     /// </code></example>
@@ -551,7 +551,7 @@ namespace AUCapture_WPF.Annotations
     /// </summary>
     /// <example><code>
     /// [Pure] int Multiply(int x, int y) => x * y;
-    /// 
+    ///
     /// void M() {
     ///   Multiply(123, 42); // Warning: Return value of pure method is not used
     /// }
@@ -591,7 +591,7 @@ namespace AUCapture_WPF.Annotations
     /// <example><code>
     /// class Foo {
     ///   [ProvidesContext] IBarService _barService = ...;
-    /// 
+    ///
     ///   void ProcessNode(INode node) {
     ///     DoSomething(node, node.GetGlobalServices().Bar);
     ///     //              ^ Warning: use value of '_barService' field
@@ -1064,7 +1064,7 @@ namespace AUCapture_WPF.Annotations
     /// {
     ///   // custom check for null but no enumeration
     /// }
-    /// 
+    ///
     /// void Foo(IEnumerable&lt;string&gt; values)
     /// {
     ///   ThrowIfNull(values, nameof(values));
